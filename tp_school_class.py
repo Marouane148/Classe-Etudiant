438216dd5316c5dc754814bef7da42ba1d1c207a
Marouane <marouanekaidi@gmail.com>
2026-03-25 13:24:22 +0100

    feat: declare Student and SchoolClass classes

---
+++ tp_school_class.py
@@ -0,0 +1,20 @@
+class Student:
+    def __init__(self, name, matter_1, matter_2, matter_3):
+        self.name = name
+        self.matter_1 = matter_1
+        self.matter_2 = matter_2
+        self.matter_3 = matter_3
+
+    def __repr__(self):
+        return (f"Student({self.name}, "
+                f"matter_1={self.matter_1}, "
+                f"matter_2={self.matter_2}, "
+                f"matter_3={self.matter_3})")
+
+
+class SchoolClass:
+    def __init__(self):
+        self.students = []
+
+    def add_student(self, student):
+        self.students.append(student)
